e57ff189418af6d0d11cc52904787e3dd3e9ed3b
Khokan Mandal <73274551+khokanmandal@users.noreply.github.com>
2022-06-03 08:28:53 +0530

    Add files via upload

---
+++ AddTwoNum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2,res;
+	printf("Enter two number : ");
+	scanf("%d%d",&num1,&num2);
+	res=num1+num2;
+	printf("The sum result is : %d",res);
+	return 0;
+}
